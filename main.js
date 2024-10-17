c9b8175a61fea622ac0a3994f53565cb0c7dcff3
jonathan aguirre <jonathanaguirre95@hotmail.com>
2024-10-17 20:11:08 -0300

    avance con local storage dom y eventos

--- main.js
+++ main.js
@@ -17,52 +17,62 @@ let autos = [
     new Auto("chevrolet", "onix", 14000, 2019)
 ];
 
-alert("Bienvenidos, a continuación se mostrará el catálogo de autos");
+// alert("Bienvenidos, a continuación se mostrará el catálogo de autos");
 
-for (const auto of autos) {
-    console.log("------------------------");
-    console.log("Marca: " + auto.marca);
-    console.log("Modelo: " + auto.modelo);
-    console.log("Precio: " + auto.precio + "$");
-    console.log("Año: " + auto.año);
-}
-let numero = Number(prompt("Desea comprar un auto ? Ingrese 1 para Sí o 2 para No"))
+// for (const auto of autos) {
+//     console.log("------------------------");
+//     console.log("Marca: " + auto.marca);
+//     console.log("Modelo: " + auto.modelo);
+//     console.log("Precio: " + auto.precio + "$");
+//     console.log("Año: " + auto.año);
+// }
+// let numero = Number(prompt("Desea comprar un auto ? Ingrese 1 para Sí o 2 para No"))
 
-while (isNaN(numero) || numero > 2 || numero < 1) {
-    alert("Ingrese un número válido");
-    numero = Number(prompt("Ingrese 1 para Sí o 2 para No"));
-}
+// while (isNaN(numero) || numero > 2 || numero < 1) {
+//     alert("Ingrese un número válido");
+//     numero = Number(prompt("Ingrese 1 para Sí o 2 para No"));
+// }
 
-if (numero == 1) {
+// if (numero == 1) {
 
-    let mostrar = prompt("Ingrese el modelo del auto que le interesa comprar");
-    let resultado = autos.find((elm) => elm.modelo === mostrar);
-    console.log(resultado);
-        if (resultado) {
-    let continuar = confirm("¿Confirma comprar un " + resultado.marca + " " + resultado.modelo + " a un valor de " + resultado.precio + "u$s");
-    if (continuar) {
-        alert("Gracias por comprar un " + resultado.marca + " " + resultado.modelo);
-        idvendido = mostrar;
-        const autosActualizados = autos.filter(autos => autos.modelo !== idvendido);
-        console.log(autosActualizados)
-        }
-    } else {
-        alert("Lo siento, no tenemos ese modelo disponible.");
+//     let mostrar = prompt("Ingrese el modelo del auto que le interesa comprar");
+//     let resultado = autos.find((elm) => elm.modelo === mostrar);
+//     console.log(resultado);
+//         if (resultado) {
+//     let continuar = confirm("¿Confirma comprar un " + resultado.marca + " " + resultado.modelo + " a un valor de " + resultado.precio + "u$s");
+//     if (continuar) {
+//         alert("Gracias por comprar un " + resultado.marca + " " + resultado.modelo);
+//         idvendido = mostrar;
+//         const autosActualizados = autos.filter(autos => autos.modelo !== idvendido);
+//         console.log(autosActualizados)
+//         }
+//     } else {
+//         alert("Lo siento, no tenemos ese modelo disponible.");
+//     }
+
+// } if (numero == 2) {
+//     alert("Muchas gracias vuelva pronto")
+
+// }
+
+
+
+
+
+
+
+
+function loguin() {
+    let usuario = document.getElementById("usuario").value;
+    let pass = document.getElementById("pass").value;
+
+    if (usuario == "jonathan9520" && pass == "2024" || usuario == "admin" && pass == "1234" ) {
+        window.location = "./autos.html"
     }
-
-} if (numero == 2) {
-    alert("Muchas gracias vuelva pronto")
-
+    else{
+        alert("Datos incorrectos, vuelva a intentarlo")
+    }
 }
-
-
-
-
-
-
-
-
-
 
 function pedir_numero() {
     let numero = Number(prompt("¿Desea comprar un auto? Ingrese 1 para Sí o 2 para No"));
@@ -72,3 +82,9 @@ function pedir_numero() {
     }
     return numero;
 }
+
+window.addEventListener("keydown",(e)=>{
+    if (e.keyCode===13) {
+        loguin();
+    }
+})
